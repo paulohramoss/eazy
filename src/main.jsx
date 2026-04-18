783c28878def741059f8b07e475dbfb806ce82d7
Paulo Ramos <pramosphdr548@gmail.com>
2026-04-18 11:08:56 -0300

    feat: implement dashboard layout with navigation, profile management, and Vercel analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import '@flaticon/flaticon-uicons/css/regular/rounded.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
